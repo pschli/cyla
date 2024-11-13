cdbe152cff66e1ce68bed80711656dd7e39e623d
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-11-13 13:08:57 +0100

    landing basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MaincontentComponent } from './maincontent/maincontent.component';
 import { SuccessComponent } from './maincontent/success/success.component';
+import { LandingComponent } from './landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', component: MaincontentComponent },
+  { path: '', component: LandingComponent },
+  { path: 'schedule', component: MaincontentComponent },
   { path: 'success', component: SuccessComponent },
 ];
