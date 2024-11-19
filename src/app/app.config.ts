c7f4fcdb9eb4d3c5112395262a997aa893edb4dc
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-11-19 10:13:24 +0100

    resgister with firebase Auth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,26 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideHttpClient } from '@angular/common/http';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGbaEoxgnaG927RB3piJD5d9OWMwONANA',
+  authDomain: 'cyla-d3d28.firebaseapp.com',
+  projectId: 'cyla-d3d28',
+  storageBucket: 'cyla-d3d28.firebasestorage.app',
+  messagingSenderId: '705817176054',
+  appId: '1:705817176054:web:242993700aba99d7e0c9d7',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
   ],
 };
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  firebaseAuth = inject(Auth);
+
+  constructor() {}
+
+  register(
+    email: string,
+    password: string,
+    firstname: string,
+    lastname: string
+  ): Observable<void> {
+    console.log(email, password);
+    let displayName = `${firstname} ${lastname}`;
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) =>
+      updateProfile(response.user, { displayName: displayName })
+    );
+
+    return from(promise);
+  }
+}
