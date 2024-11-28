2a7183f9a45b64f63818216700ca1396b1cd6af0
Patrick Schlieker <patrick.schlieker@gmail.com>
2024-11-28 10:19:17 +0100

    mat-calendar select dates event

--- src/app/admin/admin-panel/month-display/month-display.component.ts
+++ src/app/admin/admin-panel/month-display/month-display.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatCalendar,
+  MatDatepickerInputEvent,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { CalendarCustomHeader } from '../calendar-custom-header/calendar-custom-header.component';
 
@@ -13,4 +17,10 @@ import { CalendarCustomHeader } from '../calendar-custom-header/calendar-custom-
 })
 export class MonthDisplayComponent {
   readonly customHeader = CalendarCustomHeader;
+  selectedDate = new Date('11/30/2024');
+  selectedDates = [new Date('11/30/2024'), new Date('11/29/2024')];
+
+  public selectedChange(event: MatDatepickerInputEvent<Date>): void {
+    console.log('Teste', event);
+  }
 }
