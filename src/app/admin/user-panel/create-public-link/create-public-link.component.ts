8f14234b93283d7ebe756b21b05cb488f597681e
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-02-03 16:21:52 +0100

    random link

--- src/app/admin/user-panel/create-public-link/create-public-link.component.ts
+++ src/app/admin/user-panel/create-public-link/create-public-link.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import {
   FormControl,
+  FormGroupDirective,
   FormsModule,
+  NgForm,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { ErrorStateMatcher } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+
+export class LinkErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 
 @Component({
   selector: 'app-create-public-link',
@@ -25,13 +42,29 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './create-public-link.component.scss',
 })
 export class CreatePublicLinkComponent {
-  linkFormControl = new FormControl('', [Validators.required]);
+  regex = /^[a-zA-Z0-9_-]*$/;
+  linkFormControl = new FormControl('', [
+    Validators.pattern(this.regex),
+    Validators.required,
+  ]);
+
+  matcher = new LinkErrorStateMatcher();
 
   generateLink() {
-    console.log('generate Link');
+    const link = this.generateUID();
+    this.linkFormControl.setValue(link);
   }
 
   sendLink() {
-    console.log('send Link');
+    const link = this.linkFormControl.value;
+  }
+
+  generateUID(): string {
+    const array = new Uint32Array(2);
+    crypto.getRandomValues(array);
+    return (
+      array[0].toString(36).slice(0, 5).padStart(5, '0') +
+      array[1].toString(36).slice(0, 5).padStart(5, '0')
+    );
   }
 }
