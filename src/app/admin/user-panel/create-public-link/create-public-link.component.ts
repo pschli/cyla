ba6495c71657ecf6a2874c689e803b70e94fce65
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-02-06 18:19:05 +0100

    handle existing link

--- src/app/admin/user-panel/create-public-link/create-public-link.component.ts
+++ src/app/admin/user-panel/create-public-link/create-public-link.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroupDirective,
   FormsModule,
@@ -13,12 +14,11 @@ import { ErrorStateMatcher } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { DateDataService } from '../../../services/date-data.service';
-import { lastValueFrom, Observable, Subscription } from 'rxjs';
+import { lastValueFrom, merge, Observable, Subscription } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { FirestoreService } from '../../../services/firestore.service';
 import { Router } from '@angular/router';
-
-type LinkResponse = 'error' | 'saved' | 'exists';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 export class LinkErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(
@@ -48,30 +48,25 @@ export class LinkErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './create-public-link.component.html',
   styleUrl: './create-public-link.component.scss',
 })
-export class CreatePublicLinkComponent implements OnInit {
+export class CreatePublicLinkComponent {
   userDates = inject(DateDataService);
   router = inject(Router);
   fs = inject(FirestoreService);
-  linkResponse$: Observable<any> | null = null;
-  linkResponseSub?: Subscription;
   regex = /^[a-zA-Z0-9_-]*$/;
   linkFormControl = new FormControl('', [
     Validators.pattern(this.regex),
     Validators.required,
   ]);
+  triedLinks: Array<string> = [];
 
   matcher = new LinkErrorStateMatcher();
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.linkResponseSub = this.linkResponse$?.subscribe((responses) => {
-      console.log(responses);
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.linkResponseSub?.unsubscribe();
+  constructor(private http: HttpClient) {
+    merge(this.linkFormControl.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        this.checkTriedLinks();
+      });
   }
 
   generateLink() {
@@ -80,7 +75,6 @@ export class CreatePublicLinkComponent implements OnInit {
   }
 
   async sendLink() {
-    console.log(this.linkFormControl.value);
     const link = this.linkFormControl.value;
     if (link) {
       let linkState = await this.sendPublicLink(link);
@@ -105,7 +99,7 @@ export class CreatePublicLinkComponent implements OnInit {
   handleResponse(linkState: string, link: string) {
     switch (linkState) {
       case 'error':
-        console.log('error');
+        this.showDBErrorMessage();
         break;
       case 'data saved':
         console.log('data saved');
@@ -113,10 +107,34 @@ export class CreatePublicLinkComponent implements OnInit {
         this.router.navigateByUrl('admin/overview');
         break;
       case 'link exists':
-        console.log('link exists');
+        this.handleLinkExists(link);
         break;
     }
   }
+
+  handleLinkExists(link: string) {
+    this.triedLinks.push(link);
+    this.checkTriedLinks();
+  }
+
+  checkTriedLinks() {
+    if (this.triedLinks.length === 0 || !this.linkFormControl.value) return;
+    if (this.triedLinks.includes(this.linkFormControl.value)) {
+      this.setExistsError();
+    } else {
+      this.removeExistsError();
+    }
+  }
+
+  setExistsError() {
+    this.linkFormControl.setErrors({ 'exists already': true });
+  }
+
+  removeExistsError() {
+    this.removeFormControlError(this.linkFormControl, 'too early');
+  }
+
+  showDBErrorMessage() {}
 
   generateUID(): string {
     const array = new Uint32Array(2);
@@ -125,5 +143,14 @@ export class CreatePublicLinkComponent implements OnInit {
       array[0].toString(36).slice(0, 5).padStart(5, '0') +
       array[1].toString(36).slice(0, 5).padStart(5, '0')
     );
+  }
+
+  removeFormControlError(control: AbstractControl, errorName: string) {
+    if (control?.errors && control?.errors[errorName]) {
+      delete control.errors[errorName];
+      if (Object.keys(control.errors).length === 0) {
+        control.setErrors(null);
+      }
+    }
   }
 }
