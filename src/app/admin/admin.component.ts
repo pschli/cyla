59a4267d3df54a2c1a4d7b118c03c2afe3b7b1e5
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-02-27 16:42:18 +0100

    animation and placeholders

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DateDataService } from '../services/date-data.service';
+import { DurationsService } from '../services/durations.service';
 
 @Component({
   selector: 'app-admin',
