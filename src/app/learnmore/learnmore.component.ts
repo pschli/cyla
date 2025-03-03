6de9d5cca31acbe9ee60f322dcdc2a64460dd67b
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-03-03 17:41:09 +0100

    learn more structure and images

--- src/app/learnmore/learnmore.component.ts
+++ src/app/learnmore/learnmore.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-learnmore',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './learnmore.component.html',
-  styleUrl: './learnmore.component.scss'
+  styleUrl: './learnmore.component.scss',
 })
-export class LearnmoreComponent {
-
-}
+export class LearnmoreComponent {}
