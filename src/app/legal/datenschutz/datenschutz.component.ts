82d913f61a2fed6f1358dc5b0ba95e466d944135
Patrick Schlieker <patrick.schlieker@gmail.com>
2025-03-25 18:14:11 +0100

    privacy policy

--- src/app/legal/datenschutz/datenschutz.component.ts
+++ src/app/legal/datenschutz/datenschutz.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-datenschutz',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './datenschutz.component.html',
-  styleUrl: './datenschutz.component.scss'
+  styleUrl: './datenschutz.component.scss',
 })
 export class DatenschutzComponent {
-
+  constructor() {
+    window.scrollTo({ top: 0 });
+  }
 }
